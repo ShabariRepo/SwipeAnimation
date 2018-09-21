7281db70be6a9787409316cdde926b4b591f601e
shabari shenoy <sss_shenoy06@hotmail.com>
2018-09-21 16:45:19 -0400

    - extra comments to explain pan responder lifecycle - make the swipe motion if its greater than 40% of the screen width (not 25%) - stacking the cards - fix for the last one coming up is to add zIndex for i

--- src/Deck.js
+++ src/Deck.js
@@ -3,8 +3,8 @@ import { View, Animated, PanResponder, Dimensions } from 'react-native';
 
 // this is something that wont change over time so keep it outside
 const SCREEN_WIDTH = Dimensions.get('window').width;
-// thresh hold for swiping 1/4 of the screen
-const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+// thresh hold for swiping 40% of the screen
+const SWIPE_THRESHOLD = 0.40 * SCREEN_WIDTH;
 // duration of swipe out card
 const SWIPE_OUT_DURATION = 250;
 
@@ -12,8 +12,8 @@ class Deck extends Component {
     // if user of the component does not pass in the given props than it will use the ones here as default
     // always do a default as a practice, when writing re-usable components (to avoid type checking like if(onSwipeRight) do.. and avoid errors)
     static defaultProps = {
-        onSwipeRight: () => {},
-        onSwipeLeft: () => {}
+        onSwipeRight: () => { },
+        onSwipeLeft: () => { }
     }
 
     constructor(props) {
@@ -25,6 +25,8 @@ class Deck extends Component {
             /*
             onStartShouldSetPanResponder: () => {}, ========= executed anytime a user taps on the screen
             if returned true, we want this instance of the panResponder (const) to handle that gesture
+            anytime user taps on the screen
+            - so below means if user taps on screen then set to true meaning assign pan responder at that time
             
             onPanResponderMove: () => {},  ============= anytime that the user starts to drag their finger across the screen
             first argument is always called event (convention) works similar to the event in reactJS (what element is pressed down on)
@@ -33,13 +35,14 @@ class Deck extends Component {
 
             onPanResponderRelease: () => {}  =========== anytime the user removes their finger from the press screen
              */
+            // below are Pan Responder lifecycle events
             onStartShouldSetPanResponder: () => true,
             onPanResponderMove: (event, gesture) => {
                 // every time the user drags the finger take the the value and update current position
                 position.setValue({ x: gesture.dx })//to also move up and down , y: gesture.dy })
             },
             onPanResponderRelease: (event, gesture) => {
-                if (gesture.dx > SWIPE_THRESHOLD){
+                if (gesture.dx > SWIPE_THRESHOLD) {
                     // swipe right
                     this.forceSwipe('right');
                 } else if (gesture.dx < -SWIPE_THRESHOLD) {
@@ -48,6 +51,7 @@ class Deck extends Component {
                 } else {
                     this.resetPosition();
                 }
+                // gesture.numberActiveTouches: 1 indicates that 1 finger, can do something for 2 if more than one
             }
         });
 
@@ -58,7 +62,7 @@ class Deck extends Component {
         this.state = { panResponder, position, index: 0 };
     }
 
-    forceSwipe(direction){
+    forceSwipe(direction) {
         const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
         // timing is same as spring but timing is more linear
         // timing is used anytime there needs to be updates to NEW position (difference is you have to specify duration of animation)
@@ -67,17 +71,18 @@ class Deck extends Component {
             duration: SWIPE_OUT_DURATION
         }).start(() => this.onSwipeComplete(direction));
         // callback once animation is complete and pass direction
+        console.log("swiped right");
     }
 
     // reset the position of the card to default (0, 0)
-    resetPosition(){        
+    resetPosition() {
         Animated.spring(this.state.position, {
-            toValue: { x:0, y:0 }
+            toValue: { x: 0, y: 0 }
         }).start();
     }
 
     // after swiped card animation is complete (off the page)
-    onSwipeComplete(direction){
+    onSwipeComplete(direction) {
         const { onSwipeLeft, onSwipeRight, data } = this.props;
         const item = data[this.state.index];
 
@@ -96,7 +101,7 @@ class Deck extends Component {
     }
 
     // helper method to determine the styling for the top card
-    getCardStyle(){
+    getCardStyle() {
         // interpolation between the x location the card is dragged and rotation
         const { position } = this.state;  // get access to the position property
         // amount of input with the output of rotation
@@ -117,8 +122,8 @@ class Deck extends Component {
     renderCards() {
         if (this.state.index >= this.props.data.length) {
             return this.props.renderNoMoreCards();
-          }
-
+        }
+        
         // pass in the list of data (array)
         // for each item in that array it calls renderCard()
         return this.props.data.map((item, i) => {
@@ -130,33 +135,33 @@ class Deck extends Component {
             if (i === this.state.index) {
                 return (
                     //[this.getCardStyle(), styles.cardStyle]
-                  <Animated.View
-                    key={item.id}
-                    style={this.getCardStyle()}
-                    {...this.state.panResponder.panHandlers}
-                  >
-                    {this.props.renderCard(item)}
-                  </Animated.View>
+                    <Animated.View
+                        key={item.id}
+                        style={[this.getCardStyle(), styles.cardStyle, { zIndex: i * -1 }]}
+                        {...this.state.panResponder.panHandlers}
+                    >
+                        {this.props.renderCard(item)}
+                    </Animated.View>
                 );
-              }
+            }
 
             // other cards not part of previous or current index jsut make regular card
             return (
-                // style={styles.cardStyle}
-                <View key={item.id}>
-                  {this.props.renderCard(item)}
+                // style={styles.cardStyle} to stack
+                <View key={item.id} style={[styles.cardStyle, { zIndex: i * -1 }]}>
+                    {this.props.renderCard(item)}
                 </View>
-              );
-        });//.reverse();
+            );
+        }).reverse();
     }
 
     render() {
         return (
-          <View>
-            {this.renderCards()}
-          </View>
+            <View>
+                {this.renderCards()}
+            </View>
         );
-      }
+    }
 }
 
 const styles = {
